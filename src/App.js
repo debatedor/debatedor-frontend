319f699b84a189adc36c2600cd55dbab5ea3d4fe
Daniel Luiz <danielluizdossantos12@gmail.com>
2024-06-05 17:41:38 -0300

    Pagina Login Iniciada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-     
-       
+        <Header/>
       </header>
+      <body>
+        <Formulario/>
+      </body>
+      <footer>
+
+      </footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./Login.css"
+
+function Header() {
+    return (
+        <div>
+            <h1 id = "Titulo_Login"> Debatedor </h1>
+        </div>
+    );
+}
+
+export default Header
