95651dd725e145b2b1996fb84f0eeff80c1cfe53
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-06-11 11:15:02 -0300

    merge com gustavo brance

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Formulario from './components/Formulario';
+import Formulario from './components/FormularioLogin.js';
 import DetailPostWrapper from './components/DetailPostWrapper';
 import CommentDetailPage from './components/Comment';
 
